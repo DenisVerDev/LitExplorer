4278c73534a572189be3c6c5eac083109168528f
Denis Verepa <denisverepadev@gmail.com>
2025-03-29 10:40:59 +0200

    added DI and Services

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using LitExplorer.Components;
+using LitExplorer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<MetadataService>();
+builder.Services.AddScoped<BrowseService>();
 
 var app = builder.Build();
 
