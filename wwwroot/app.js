a20aac9db1d92043e86f65c3f3537d5ef4d96211
Denis Verepa <denisverepadev@gmail.com>
2025-04-06 19:35:21 +0300

    Squashed commit of the following:
    commit 8b9118bfc57ee5e682bf6504a1b13f3d04e04092
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Sun Apr 6 19:34:47 2025 +0300
    
        theme toggle logic
    
    commit 07ac900294bfe4c9c71d66ea114d604bf8d2fc1a
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Sun Apr 6 14:02:32 2025 +0300
    
        dark-theme styling
    
    commit 2ce53ed8722ed503b64e297e9c12a453abc7fea8
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Fri Apr 4 18:40:39 2025 +0300
    
        global css and day theme
    
    commit 00075db5b90bf9ada0321d2dc475eb29bba102d0
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Fri Apr 4 15:42:44 2025 +0300
    
        cleaning css, more Bootstrap
    
    commit 514f0f306e21842f83bb44ddc12702a1c87fbddf
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Fri Apr 4 12:05:00 2025 +0300
    
        changed base visual design

---
+++ wwwroot/app.js
@@ -0,0 +1,8 @@
+﻿window.toggleTheme = (isDarkTheme) => {
+    if (isDarkTheme) {
+        document.body.classList.add("dark-theme");
+    }
+    else {
+        document.body.classList.remove("dark-theme");
+    }
+};
