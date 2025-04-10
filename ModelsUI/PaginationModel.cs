f1da297ebd330fac0c47dd9f88ee51c3944342c3
Denis Verepa <denisverepadev@gmail.com>
2025-04-10 15:57:09 +0300

    Pagination core implemented

---
+++ ModelsUI/PaginationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LitExplorer.ModelsUI
+{
+    public class PaginationModel
+    {
+        public int PageSize { get; init; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get;set; }
+    }
+}
