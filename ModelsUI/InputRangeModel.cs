ae0d3235ee912b964496009ee94d04e4e012eb7e
Denis Verepa <denisverepadev@gmail.com>
2025-04-11 15:18:02 +0300

    Squashed commit of the following:
    commit f1da297ebd330fac0c47dd9f88ee51c3944342c3
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Thu Apr 10 15:57:09 2025 +0300
    
        Pagination core implemented
    
    commit 1a0b892a4d82d611ffd986e547a24579989320ed
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Wed Apr 9 16:20:05 2025 +0300
    
        SortByPanel logic and design
    
    commit 53baefccded50a0670863aa88c03178df003f506
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Tue Apr 8 20:39:01 2025 +0300
    
        TagsPanel logic
    
    commit 5cb1017d57952f53fa94a5d3e46bb1b9f2026f54
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Tue Apr 8 19:29:16 2025 +0300
    
        SearchBar filter integration
    
    commit fa385cb1272923add34ce2efd975f9b3e5e82db1
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Tue Apr 8 19:22:50 2025 +0300
    
        FilterPanel reset function
    
    commit 60e862216c7ec55c242c49c6ee59a0dba8061b5c
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Tue Apr 8 19:15:32 2025 +0300
    
        FilterPanel apply filters
    
    commit e0adbf67ae3e1e0e87aeada6d6b49b6cafdec32f
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Tue Apr 8 15:49:46 2025 +0300
    
        InputRange double type support
    
    commit 138efe5fd65a533dee585bbdd290f9428afb36de
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Tue Apr 8 14:50:21 2025 +0300
    
        InputRange component
    
    commit 813b54a03a55d7d9d8e833787e2a1ab7d9d9b863
    Author: Denis Verepa <denisverepadev@gmail.com>
    Date:   Tue Apr 8 12:23:24 2025 +0300
    
        Checkbox component

---
+++ ModelsUI/InputRangeModel.cs
@@ -0,0 +1,34 @@
+﻿namespace LitExplorer.ModelsUI
+{
+    public class InputRangeModel
+    {
+        public string Name { get; set; } = "Default input range";
+
+        public double MinLimit { get; set; } = 0;
+        public double MaxLimit { get; set; } = double.MaxValue;
+
+        public double? MinValue 
+        { 
+            get => minValue;
+            set
+            {
+                if (value.HasValue)
+                    minValue = Math.Clamp(value.Value, MinLimit, MaxLimit - 1);
+                else minValue = null;
+            }
+        }
+        public double? MaxValue 
+        { 
+            get => maxValue;
+            set
+            {
+                if (value.HasValue)
+                    maxValue = Math.Clamp(value.Value, MinLimit + 1, MaxLimit);
+                else maxValue = null;
+            }
+        }
+
+        private double? minValue;
+        private double? maxValue;
+    }
+}
