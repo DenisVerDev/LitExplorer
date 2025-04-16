8c0d38f6a314b4b52ed6f267ec63389b40207d7f
Denis Verepa <denisverepadev@gmail.com>
2025-04-16 11:45:52 +0300

    StorageService implemented

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddHttpClient();
 builder.Services.AddScoped<MetadataService>();
 builder.Services.AddScoped<BrowseService>();
 builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<StorageService>();
 
 var app = builder.Build();
 
