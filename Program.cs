fa76902cc917afcab617ccd31d30aa5aab0d9d4d
Denis Verepa <denisverepadev@gmail.com>
2025-05-14 13:29:44 +0300

    LibraryServise realised

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddHttpClient();
 builder.Services.AddScoped<MetadataService>();
 builder.Services.AddScoped<BrowseService>();
 builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<LibraryService>();
 
 var app = builder.Build();
 
