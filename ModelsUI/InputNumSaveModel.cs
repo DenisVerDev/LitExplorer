d5dd797a1af30acf5155ed23c506a00997ef945e
Denis Verepa <denisverepadev@gmail.com>
2025-06-01 18:28:48 +0300

    reading history saving

---
+++ ModelsUI/InputNumSaveModel.cs
@@ -0,0 +1,20 @@
+﻿namespace LitExplorer.ModelsUI
+{
+    public class InputNumSaveModel
+    {
+        public string Name { get; set; } = "Default input";
+        public int MinLimit { get; set; } = 0;
+        public int MaxLimit { get; set; } = int.MaxValue;
+
+        private int _value;
+        public int Value
+        {
+            get => _value;
+            set
+            {
+                // Clamp the value within [MinLimit, MaxLimit]
+                _value = Math.Clamp(value, MinLimit, MaxLimit);
+            }
+        }
+    }
+}
